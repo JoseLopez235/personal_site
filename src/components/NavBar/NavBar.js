50081add2d5382601e52be7b4195a330bb8532cc
Jose Lopez <joselopez11394@gmail.com>
2021-07-19 17:18:30 -0400

    adds makshift logo and adds navbar style/css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,12 +5,29 @@ import Typography from '@material-ui/core/Typography';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
+import logo from "../NavBar/logo.png";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    boxShadow: 'none'
+  },
+  logo: {
+    maxWidth: '80px'
+  }
+}))
 
 const NavBar = () => {
+  const classes = useStyles();
+
   return (
-    <AppBar>
+    <AppBar position="absolute" className={classes.root} color="transparent">
       <Toolbar>
-        
+        <Link className={classes.root} component={RouterLink} to='/'> 
+          <img src={logo} className={classes.logo} /> 
+        </Link>
+
         <Button component={RouterLink} to={"/"}>
           Home
         </Button>
